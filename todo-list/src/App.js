02ff8ce3b88ca57ddc5e477423146dac613a8658
harunaltun38 <harunaltun38@gmx.de>
2021-02-13 14:34:07 +0100

    Realisiert die Änderung der Zustände via checkbox

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,36 +1,47 @@
 import React, { Component } from "react";
-import Todos from './components/Todos'
+import Todos from "./components/Todos";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Work Out",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Arbeiten",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Kino besuchen",
+        completed: false,
+      },
+    ],
+  };
 
-state={
-  todos:[
-    {
-      id:1,
-      title:'Work Out',
-      completed:false
-    },
-    {
-      id:2,
-      title:'Arbeiten',
-      completed:true
-    },
-    {
-      id:3,
-      title:'Kino besuchen',
-      completed:false
-    }
-  ]
-}
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        <Todos todos= {this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
 }
+
 
 export default App;
