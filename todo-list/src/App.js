421f285c8b7fad0863c6b4c44ebb228cc37ce3e4
harunaltun38 <harunaltun38@gmx.de>
2021-02-13 17:06:54 +0100

    Fügt AddTodo Komponente hinzu und dessen Styling

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
+import AddTodo from "./AddTodo";
 import "./App.css";
 
 class App extends Component {
@@ -47,12 +48,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          deleteTodo={this.deleteTodo}
-        />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            deleteTodo={this.deleteTodo}
+          />
+        </div>
       </div>
     );
   }
